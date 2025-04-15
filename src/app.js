c30e70e070cfbc9d9324eda851cb6cb3c9b48afe
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-15 00:08:54 +0000

    create card functions

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //What are the CardNumbers in a suit of cards?
+const CARDNUMBER = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const SUIT = ["DIAMONDS", "SPADES", "HEARTS", "CLUBS"];
+let randomCardNumber = Math.floor(Math.random()*CARDNUMBER.length);
+let randomSuitNumber = Math.floor(Math.random()*SUIT.length);
+let finalSuite = SUIT[randomSuitNumber];
+
+  
 };
